9427d54d613e185524f288e782c3edca59b518b8
Sumatran Beans <sumatranbeans@gmail.com>
2025-12-26 07:41:34 +0000

    fix: clerk public routes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+export default clerkMiddleware({
+  publicRoutes: ['/login', '/'],
+})
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 }
